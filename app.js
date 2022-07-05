a51ea2017962dfbfff6a1ae63f232341ae953b0d
ted <mairurat@gmail.com>
2022-07-05 22:06:18 +0300

    finished on register user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+var bcrypt = require('bcryptjs');
+var jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -11,8 +13,53 @@ app.use(express.json());
 const User = require("./model/user");
 
 //Register
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
+    try{
+        //Get user input
+        const { firstName, lastName, email, password } = req.body;
 
+        //Validate user input
+        if(!(email && password && firstName && lastName)) {
+            res.status(400).send("All input is required");
+        };
+
+        //check if user already exists
+        //Validate if user exists in our database
+        const oldUser = await User.findOne({ email });
+
+        if(oldUser) {
+            return res.status(409).send("User Already exists. Please Login 🐶 ");
+        }
+
+        //Encrypt user password
+        encryptedUserPassword = await bcrypt.hash(password, 10);
+
+        //Create user in our database
+        const user = await User.create({
+            first_name: firstName,
+            last_name: lastName,
+            email: email.toLowerCase(),
+            password: encryptedUserPassword,
+        });
+
+        //Create token
+        const token = jwt.sign(
+
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "5h",
+            }
+        );
+
+        //save user token 
+        user.token = token;
+
+        //return new user{ user_id: user._id, email },
+        res.status(201).json(user);
+    }catch(err){
+        console.log(err);
+    }
 });
 
 //Login
