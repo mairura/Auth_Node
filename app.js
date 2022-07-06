74f4c3a984c1ebd5381f3d60c943ecd5eb50aa0c
ted <mairurat@gmail.com>
2022-07-06 12:04:15 +0300

    successful login of user

--- app.js
+++ app.js
@@ -63,8 +63,40 @@ app.post("/register", async (req, res) => {
 });
 
 //Login
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+    try{
+        //Get user input
+        const { email, password } = req.body;
 
+        ////Validate user input
+        if(!(email && password)) {
+            res.status(400).send("All input is required 🤣 ")
+        }
+
+        //Validate if user exists in database
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            //Create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "5h",
+                }
+            );
+
+            //save user token
+            user.token = token;
+
+            //user
+            return res.status(200).json(user);
+        }
+        return res.status(400).send("Invalid Credentials 😢 ")
+
+    }catch(err){
+        console.log(err);
+    }
 });
 
 module.exports = app;
